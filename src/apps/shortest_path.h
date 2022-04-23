f33e868b91b23cf216cc073b0fb7d4b48f4e6871
Ajay Shridhar Joshi <ajsj7598@gmail.com>
2022-04-23 12:27:57 -0500

    Fix shortest path app data types

--- src/apps/shortest_path.h
+++ src/apps/shortest_path.h
@@ -4,14 +4,12 @@
 #include "protos/graph.grpc.pb.h"
 #include "base_app.h"
 
-using graph::Double;
-
-class ShortestPath : public BaseApp<Double, Double> {
+class ShortestPath : public BaseApp<double, double> {
 public:
     ShortestPath(std::string& graph_file, int64_t source_vtx_id);
-    void init(Vertex<Double, Double> & vertex) override;
-    void gather(Vertex<Double, Double> & src, Vertex<Double, Double>& dst, const Edge& edge) override;
-    void apply(Vertex<Double, Double> & vertex) override;
+    void init(Vertex<double, double> & vertex) override;
+    void gather(Vertex<double, double> & src, Vertex<double, double>& dst, const Edge& edge) override;
+    void apply(Vertex<double, double> & vertex) override;
 
 private:
     int64_t src_vtx_id_;
