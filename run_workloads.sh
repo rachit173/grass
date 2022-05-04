b64c5e8110b0a59dbe128215f0b245ac8e712258
rachit173 <rachittibrewal@outlook.com>
2022-05-04 12:39:21 -0500

    Single Machine, multi core workload runner with thread pinning

--- run_workloads.sh
+++ run_workloads.sh
@@ -1,9 +1,19 @@
 # Change to base directory
 BASE_DIR=/mnt/Work/grass
+SOCKET_OFFSET=16
 echo "$BASE_DIR"
 cd $BASE_DIR
 bazel build -c opt //src:run_app
 
+RunCore () {
+  rank=$1
+  ./bazel-bin/src/run_app $rank $BASE_DIR/tmp_exec.conf &
+  pid=$(echo $!)
+  taskset -p $pid -c $rank,$(($rank+$SOCKET_OFFSET)) 
+  echo "Process $rank: $pid"
+}
+
+echo "Runnning single machine, 2 cores"
 rm /mnt/Work/grass/tmp_exec.conf
 cat >> /mnt/Work/grass/tmp_exec.conf << EOF
 buffer.capacity=4
@@ -16,13 +26,27 @@ app.base_dir=/mnt/Work/grass/resources/graphs
 app.iterations=30
 app.graph_file=web-BerkStan.txt
 EOF
-./bazel-bin/src/run_app 0 $BASE_DIR/tmp_exec.conf &
-proc1=$(echo $!)
-taskset -p $proc1 -c 0,16
-echo "Process 1: $proc1"
+RunCore 0
+RunCore 1
+wait
 
-./bazel-bin/src/run_app 1 $BASE_DIR/tmp_exec.conf &
-proc2=$(echo $!)
-echo "Process 2: $proc2"
-taskset -p $proc2 -c 1,17
+
+echo "Runnning single machine, 4 cores"
+rm /mnt/Work/grass/tmp_exec.conf
+cat >> /mnt/Work/grass/tmp_exec.conf << EOF
+buffer.capacity=2
+buffer.num_partitions=8
+buffer.num_workers=4
+buffer.server_addresses=localhost:50051,localhost:50052,localhost:50053,localhost:50054
+
+app.name=pagerank
+app.base_dir=/mnt/Work/grass/resources/graphs
+app.iterations=30
+app.graph_file=web-BerkStan.txt
+EOF
+
+RunCore 0
+RunCore 1
+RunCore 2
+RunCore 3
 wait
